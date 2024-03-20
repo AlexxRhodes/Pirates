81dfece6e7d86194754b7aa4880ba75c86e60fbb
AlexxRhodes <alex.rhodes@univ-tlse3.fr>
2024-03-20 22:55:29 +0100

    Update CaseEffetPosition.java

--- src/commande/CaseEffetPosition.java
+++ src/commande/CaseEffetPosition.java
@@ -1,5 +1,19 @@
 package commande;
 
 public class CaseEffetPosition {
-
+	public void retourCaseDepart(Joueur j) {
+		
+	}
+	
+	public void retourCaseDepart(Joueur j) {
+		
+	}
+	
+	public void retourCaseDepart(Joueur j) {
+		
+	}
+	
+	public void retourCaseDepart(Joueur j) {
+		
+	}
 }
